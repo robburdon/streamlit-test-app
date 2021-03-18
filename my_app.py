7b745650e2c3a8796568a4f9110d62c43d787aeb
robburdon <40874781+robburdon@users.noreply.github.com>
2021-03-18 13:26:43 +0000

    second commit, made use huggingface

--- my_app.py
+++ my_app.py
@@ -1,23 +1,58 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import json
+import requests
+
+#secrets
+API_TOKEN = ""
+with open('api_token.txt') as reader:
+    API_TOKEN = reader.readline().replace('\n', '')
 
 
 
+
+#huggingface stuff
+API_URL = "https://api-inference.huggingface.co/models/gpt2"
+#API_URL = "https://api-inference.huggingface.co/models/iarfmoose/t5-base-question-generator"
+#API_URL = "https://api-inference.huggingface.co/models/abbas/gpt2-horror-stories"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def query(payload):
+    data = json.dumps(payload)
+    response = requests.request("POST", API_URL, headers=headers, data=data, parameters = )
+    return json.loads(response.content.decode("utf-8"))
+
+#data = query("Can you please let us know more details about your ")
+#get the string out of the returned json
+#returned_string = data[0]["generated_text"]
+
+
+
+#chatting shit for model prompts
+
+context2 = """The most merciful thing in the world is the inability of the human mind to correlate all its contents. We live on a placid island of ignorance in the midst of black seas of infinity, and it was not meant that we should voyage far. The sciences, have hitherto harmed us little; but some day the piecing together of dissociated knowledge will open up such terrifying vistas of reality, and of our frightful position therein, that we shall either go mad from the revelation or flee from the deadly light into the peace and safety of a new dark age.
+"""
+
+context = "some day the piecing together of dissociated knowledge will open up such terrifying vistas of reality, and of our frightful position therein, that we shall either go mad from the revelation or flee from the deadly light into the peace and safety of a new dark age."
+
+
+
+#app
 st.title('What have I done')
 
-st.write("Do you like my table")
+user_input = st.text_area('GPT2 but it is haunted.')
 
-data = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': ["I", "have", "a", "problem"]
-})
+if st.button('Shoot'):
+    prompt = user_input
+    data = query(prompt)
+    returned_string = data #[0]["generated_text"]
+    st.write(returned_string)
 
-st.write(data)
 
-st.write("And this is where the spiders are:")
+#st.write("And this is where the spiders are:")
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [50.07, 14.4],
     columns=['lat', 'lon'])
 
-st.map(map_data)
+#st.map(map_data)
